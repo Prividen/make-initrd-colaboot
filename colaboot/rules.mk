d9a5641102a70e5ee9524cc117eaa2c88c2df57a
Michael A. Kangin <prividen@altlinux.org>
2019-01-22 19:42:56 +0100

    Do not add any kernel modules to initrd image

--- colaboot/rules.mk
+++ colaboot/rules.mk
@@ -1,7 +1,6 @@
-MODULES_ADD += af_packet overlay squashfs
 colaboot:
 	@echo "Adding support for CoLaBoot ..."
 	@put-file "$(ROOTDIR)" $(COLABOOT_FILES)
 	@put-tree "$(ROOTDIR)" $(COLABOOT_DATADIR)
-
+	@bash -c '[ -d "$$WORKDIR/img/lib/modules" ] && rm -rf "$$WORKDIR/img/lib/modules"; true; '
 pack: colaboot
